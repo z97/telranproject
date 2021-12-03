87f6115be3bece92152a2b4ad7944e054ec8f4ea
kliba <klibaner@mail.com>
2021-12-03 14:44:02 +0200

    9

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Search from "./Search";
 import SmallEvent from "./SmallEvent";
+import Header from "./Header";
 
 const Body = () => {
     return (
         <div className='body'>
-            <Search/>
-            <SmallEvent/>
+            <Header/>
+            <div className="d-flex flex-column mb-3">
+                <div className="p-2">Flex item 1</div>
+                <div className="p-2">Flex item 2</div>
+                <div className="p-2">Flex item 3</div>
+            </div>
         </div>
     )
 }
+export default Body;
