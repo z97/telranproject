1e95a6751766b7fa1c12345e19be1303779a8e9b
ArseniyX <jkl@usa.com>
2021-12-08 18:24:59 +0200

    deleted unused import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
-       <Body/>
-
+      <Body />
     </div>
   );
 }
