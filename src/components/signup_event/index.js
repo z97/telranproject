c1499361b824e36923956ef95c9db06a40c4a1a3
kliba <klibaner@mail.com>
2021-12-08 11:56:29 +0200

    artemPC11

--- src/components/signup_event/index.js
+++ src/components/signup_event/index.js
@@ -8,18 +8,26 @@ import SmallEvent from "../SmallEvent";
     }}>
         <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input type="name" className="form-control" id="nameInput" aria-describedby="name">
+            <input type="text" className="form-control" id="nameInput" aria-describedby="name">
             </input>
         </div>
         <div className="mb-3">
             <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"></input>
+            <input type="text" className="form-control" id="inputphoneNumber">
+            </input>
         </div>
-        <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1">
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label></input>
+        <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="text" className="form-control" id="inputEmail">
+            </input>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <div className="mb-3">
+            <label htmlFor="count" className="form-label">Email</label>
+            <input type="number" className="form-control" id="inputCount">
+            </input>
+        </div>
+        <p>Attention. Be careful when filling out ... </p>
+        <button type="submit" className="btn btn-primary">Payment</button>
     </form>
 </div>
 
